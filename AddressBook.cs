6a20ba60aba2bfba20cc41025036b49753d8aaf4
warrior14 <ironaddicts14@gmail.com>
2021-08-27 19:27:17 -0500

    initial commit

---
+++ AddressBook.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+
+namespace AddressBook 
+{
+    public class AdressBook 
+    {
+        public Dictionary<string, Contact> Phonebook { get; set; }
+
+        public AddressBook()
+        {
+            PhoneBook = new Dictionary<string, Contact>();
+        }
+
+        // method to add a Contact object param to the AddressBook class with the email address as the key and the value as the contact object
+        public void AddContact(Contact personContact)
+        {
+            try 
+            {
+                Phonebook.Add(personContact.Email, personContact);
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine($"You've attempted to add a duplicate contact with email: {personContact.Email}. Please try to add a new contact!");
+            }
+        }
+        // method to receive contact by email
+        public Contact GetEmail(string email)
+        {
+            
+        }
+    }
+}
